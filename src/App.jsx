c9c7f5326ae874749884033f5848c2c94aaf05d7
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-12-09 18:53:31 -0500

    Added more pages and modules + nested routes + index route

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Homepage />} />
-<Route path='product' element={<Product />} />
-<Route path="pricing" element={<Pricing />} />
-<Route path="app" element={<AppLayout />} />
-<Route path="*" element={<PageNotFound />} />
-
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
