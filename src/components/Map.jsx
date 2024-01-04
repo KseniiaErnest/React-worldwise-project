5467fd613f3f488d3ff931bf3d23fd94d4eb78d2
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-01-04 17:06:13 -0500

    setting map position + fetching data in the form + creating city + deleting city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,24 +1,36 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css';
 import { MapContainer, TileLayer, Popup, Marker, useMap, useMapEvents } from 'react-leaflet';
+import Button from "./Button";
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 export default function Map() {
  
-  const [mapPoistion, setMapPosition] = useState([40, 0]);
-  const { cities } = useCities();
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const { cities } = useCities();
+  const [mapPoistion, setMapPosition] = useState([40, 0]);
+  const {isLoading: isLoadingPosition, position: geolocationPosition, getPosition,} = useGeolocation();
+
+  const [mapLat, mapLng] = useUrlPosition();
 
 useEffect(function() {
   if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
-  }, [mapLat, mapLng] )
+  }, [mapLat, mapLng] );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+    <div className={styles.mapContainer}>
+    {!geolocationPosition && <Button type='position' onClick={getPosition}>{isLoadingPosition ? 'Loading...' : 'Use your position'}</Button>}
       <MapContainer center={mapPoistion} zoom={6} scrollWheelZoom={true} className={styles.map}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -39,10 +51,19 @@ useEffect(function() {
   )
 }
 
+// function ChangeCenter({position}) {
+// const map = useMap();
+// map.setView(position);
+// return null;
+// }
 function ChangeCenter({position}) {
-const map = useMap();
-map.setView(position);
-return null;
+  const map = useMap();
+
+  if (position) {
+    map.setView(position);
+  }
+
+  return null;
 }
 
 function DetectClick() {
